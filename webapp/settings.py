386e2c779ddbfbcf684385a1b6f3552758eb5ae5
Jaheimram <Jaheimram@gmail.com>
2025-04-21 19:12:10 -0500

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hzej)p&qv39o%th*+*7j7so=2vw%+!e&h+78@d%j4p(k*#z2t)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safe-hardhat-sensor.onrender.com']
 
 
 # Application definition
